a9ea4a749e4af78e75d5ffd4f2ae4f55c423e066
Y4tendouji <pedrolucassouzacarneiro@outlook.com>
2025-12-13 15:32:02 -0300

    fix: changed 279B to the right directory

---
+++ Techniques/two_pointers/codeforces/977F - Consecutive Subsequence.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <unordered_map>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+
+
+
+void solve() {
+    
+    }
+    
+    
+    
+
+
+    
+    
+
+
+int main() {
+
+        solve();
+
+    return 0;
+}
